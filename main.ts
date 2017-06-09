7c543b8f75aa62f6b279d14afcac12844dd018fc
Adam Pasternack <adam.pasternack@gmail.com>
2017-06-09 16:19:51 -0400

    map & filter operators

--- main.ts
+++ main.ts
@@ -3,13 +3,22 @@ import { Observable, Observer } from "rxjs";
 let numbers = [1, 5, 10];
 let source = Observable.create(observer => {
 
-    for(let n of numbers) {
-        observer.next(n);
+    let index = 0;
+    let produceValue = () => {
+        observer.next(numbers[index++]);
+
+        if(index < numbers.length) {
+            setTimeout(produceValue, 250);
+        }
+        else {
+            observer.complete();
+        }
     }
 
-    observer.complete();
+    produceValue();
 
-});
+}).map(n => n * 2)
+    .filter(n => n > 4);
 
 source.subscribe(
     value => console.log(`value: ${value}`),
