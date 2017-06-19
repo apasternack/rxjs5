da0d6dc37adecceb5a4f660badf43a450a83322e
Adam Pasternack <adam.pasternack@gmail.com>
2017-06-19 10:43:30 -0400

    making an observable from a DOM event

--- main.ts
+++ main.ts
@@ -5,27 +5,13 @@ import { Observable, Observer } from "rxjs";
 // import "rxjs/add/operator/filter";
 
 let numbers = [1, 5, 10];
-let source = Observable.create(observer => {
+let source = Observable.fromEvent(document, "mousemove")
 
-    let index = 0;
-    let produceValue = () => {
-        observer.next(numbers[index++]);
-
-        if(index < numbers.length) {
-            setTimeout(produceValue, 250);
-        }
-        else {
-            observer.complete();
-        }
-    }
-
-    produceValue();
-
-}).map(n => n * 2)
-    .filter(n => n > 4);
+    // .map(n => n * 2)
+    // .filter(n => n > 4);
 
 source.subscribe(
-    value => console.log(`value: ${value}`),
+    value => console.log(value),
     e => console.log(`error: ${e}`),
     () => console.log("complete")
 );
