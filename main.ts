b4f31c0f66823b8878aae023df1549f76d8243c5
Adam Pasternack <adam.pasternack@gmail.com>
2017-06-19 13:51:14 -0400

    Creating an observable from a mock http request, V1

--- main.ts
+++ main.ts
@@ -4,27 +4,32 @@ import { Observable, Observer } from "rxjs";
 // import "rxjs/add/operator/map";  //this file has side effects which add .map to Observable
 // import "rxjs/add/operator/filter";
 
-let circle = document.getElementById("circle");
-let source = Observable.fromEvent(document, "mousemove")
-                        .map((e : MouseEvent) => {
-                            return {
-                                x: e.clientX,
-                                y: e.clientY
-                            }
-                        })
-                        .filter(value => value.x < 500)
-                        .delay(300);
+let output = document.getElementById("output");
+let button = document.getElementById("button");
+
+let click = Observable.fromEvent(button, "click");
 
     // .map(n => n * 2)
     // .filter(n => n > 4);
 
-function onNext(value) {
-    circle.style.left = value.x;
-    circle.style.top = value.y;
+function load(url: string) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("load", () => {
+        let movies = JSON.parse(xhr.responseText);
+        movies.forEach(m => {
+            let div = document.createElement("div");
+            div.innerText = m.title;
+            output.appendChild(div);
+        });
+    });
+
+    xhr.open("GET", url);
+    xhr.send();
 }
 
-source.subscribe(
-    onNext,
+click.subscribe(
+    e => load("movies.json"),
     e => console.log(`error: ${e}`),
     () => console.log("complete")
 );
