6dc75115392ce340adc91976c4f6335d0bbe713c
Adam Pasternack <adam.pasternack@gmail.com>
2017-06-19 14:08:09 -0400

    mapping an observable into an observable

--- main.ts
+++ main.ts
@@ -9,27 +9,37 @@ let button = document.getElementById("button");
 
 let click = Observable.fromEvent(button, "click");
 
-    // .map(n => n * 2)
-    // .filter(n => n > 4);
+// .map(n => n * 2)
+// .filter(n => n > 4);
 
 function load(url: string) {
-    let xhr = new XMLHttpRequest();
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
 
-    xhr.addEventListener("load", () => {
-        let movies = JSON.parse(xhr.responseText);
-        movies.forEach(m => {
-            let div = document.createElement("div");
-            div.innerText = m.title;
-            output.appendChild(div);
+        xhr.addEventListener("load", () => {
+            let data = JSON.parse(xhr.responseText);
+            observer.next(data);
+            observer.complete();
         });
-    });
 
-    xhr.open("GET", url);
-    xhr.send();
+        xhr.open("GET", url);
+        xhr.send();
+    });
 }
 
-click.subscribe(
-    e => load("movies.json"),
-    e => console.log(`error: ${e}`),
-    () => console.log("complete")
-);
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
+
+click.map(e => load("movies.json"))
+    .subscribe(o => console.log(o));
+
+// click.subscribe(
+//     e => load("movies.json"),  //in RXJS world this step becomes the creating of an observable
+//     e => console.log(`error: ${e}`),
+//     () => console.log("complete")
+// );
